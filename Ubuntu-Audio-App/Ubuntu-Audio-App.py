9a191ec31ffca3005e119ce55bc0113a8954cff1
equal8888 <kimi.savander@gmail.com>
2018-04-24 18:08:08 +0300

    app

--- Ubuntu-Audio-App/Ubuntu-Audio-App.py
+++ Ubuntu-Audio-App/Ubuntu-Audio-App.py
@@ -8,8 +8,8 @@ import os
 class AppGUI1:
     def __init__(self, master):
         window = master
-        master.minsize(width=775, height=194)
-        master.maxsize(width=775, height=194)
+        master.minsize(width=775, height=225)
+        master.maxsize(width=775, height=225)
         master.title("Ubuntu Audio App")
         master.geometry("100x500")
 
@@ -73,7 +73,7 @@ class AppGUI1:
 
         SetRe1_button = Button(text="Default Settings", command=self.SetRe1)
         SetRe1_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
-        SetRe1_button.place(relx=1, x=-774, y=165)
+        SetRe1_button.place(relx=1, x=-774, y=195)
 
         SetRe3_button = Button(master, text="medium quality", command=self.SetRe3)
         SetRe3_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
@@ -81,11 +81,11 @@ class AppGUI1:
 
         SetRe4_button = Button(master, text="best quality", command=self.SetRe4)
         SetRe4_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
-        SetRe4_button.place(relx=1, x=-374, y=134)
+        SetRe4_button.place(relx=1, x=-372, y=134)
 
         SetRe5_button = Button(master, text="zero order hold", command=self.SetRe5)
         SetRe5_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
-        SetRe5_button.place(relx=1, x=-275, y=134)
+        SetRe5_button.place(relx=1, x=-273, y=134)
 
         SetRe10_button = Button(master, text="ffmpeg", command=self.SetRe10)
         SetRe10_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
@@ -103,18 +103,36 @@ class AppGUI1:
         ttk.Separator(root).place(x=0, y=163, relwidth=10)
 
         fm5 = Frame(master)
+        label = Label(master, text="Predefined settings")
+        label.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
+        label.place(relx=1, x=-770, y=171)
+
+        Class1_button = Button(master, text="USB Audio: Class 1", command=self.Class1)
+        Class1_button.pack()
+        Class1_button.place(relx=1, x=-649, y=195)
+
+        Class2_button = Button(master, text="USB Audio: Class 2", command=self.Class2)
+        Class2_button.pack()
+        Class2_button.place(relx=1, x=-508, y=195)
+
         Test_button = Button(master, text="Show current sample rate", command=self.Test)
         Test_button.pack()
         Test_button.place(relx=1, x=-2, y=2, anchor=NE)
 
         Confirm_button = Button(master, text="   Apply !   ", command=self.Confirm)
         Confirm_button.pack()
-        Confirm_button.place(relx=1, x=-160, y=165)
+        Confirm_button.place(relx=1, x=-160, y=195)
 
         close_button = Button(master, text="Close ", command=master.quit)
         close_button.pack()
-        close_button.place(relx=1, x=-65, y=165)
+        close_button.place(relx=1, x=-65, y=195)
         fm5.pack(fill=BOTH)
+
+    def Class1(self):
+           subprocess.call('currentbitrate=$(grep "default-sample-format" /etc/pulse/daemon.conf) && sudo sed -i "/${currentbitrate}/ c default-sample-format = s24le" /etc/pulse/daemon.conf && currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 96000" /etc/pulse/daemon.conf && currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 96000" /etc/pulse/daemon.conf', shell=True)
+
+    def Class2(self):
+           subprocess.call('currentbitrate=$(grep "default-sample-format" /etc/pulse/daemon.conf) && sudo sed -i "/${currentbitrate}/ c default-sample-format = s24le" /etc/pulse/daemon.conf && currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 192000" /etc/pulse/daemon.conf && currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 192000" /etc/pulse/daemon.conf && currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = src-sinc-best-quality" /etc/pulse/daemon.conf', shell=True)
 
     def Setbit1(self):
            subprocess.call('currentbitrate=$(grep "default-sample-format" /etc/pulse/daemon.conf) && sudo sed -i "/${currentbitrate}/ c default-sample-format = s16le" /etc/pulse/daemon.conf', shell=True)
