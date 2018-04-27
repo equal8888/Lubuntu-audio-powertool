aeb535054767f2a38ee8e478d0328712ebdb89a0
equal8888 <kimi.savander@gmail.com>
2018-04-27 12:39:47 +0300

    app

--- Ubuntu-Audio-App/Ubuntu-Audio-App.py
+++ Ubuntu-Audio-App/Ubuntu-Audio-App.py
@@ -1,215 +1,147 @@
-#!/usr/bin/env python3
-import tkinter
+import tkinter as tk
 from tkinter import *
-from tkinter import ttk, Tk, Label, Button, Entry, StringVar, DISABLED, NORMAL, END, E, W
+from tkinter import ttk
 import subprocess
-import os
 
-class AppGUI1:
-    def __init__(self, master):
-        window = master
-        master.minsize(width=775, height=225)
-        master.maxsize(width=775, height=225)
-        master.title("Ubuntu Audio App")
-        master.geometry("100x500")
 
-        fm = Frame(master) # bg="blue"
+# Bit depth Button functions
+def select_bitdepth_(var):
+    selection = var.get()
+    print('Bit depth', selection)
 
-        filename = PhotoImage(file = "desk03.gif")
-        background_label = Label(fm, image=filename)
-        background_label.image = filename # anchor
-        background_label.place(x=50, y=0, relwidth=1, relheight=1)
-        background_label.place()
+    text_dict = {
+        0: 'Nothing selected!',
+        16: subprocess.call('currentbitrate=$(grep "default-sample-format" /etc/pulse/daemon.conf) && sudo sed -i "/${currentbitrate}/ c default-sample-format = s16le" /etc/pulse/daemon.conf', shell=True),
+        24: subprocess.call('currentbitrate=$(grep "default-sample-format" /etc/pulse/daemon.conf) && sudo sed -i "/${currentbitrate}/ c default-sample-format = s24le" /etc/pulse/daemon.conf', shell=True),
+        32: subprocess.call('currentbitrate=$(grep "default-sample-format" /etc/pulse/daemon.conf) && sudo sed -i "/${currentbitrate}/ c default-sample-format = s32le" /etc/pulse/daemon.conf', shell=True)
+        }
 
+    text_to_print = text_dict[selection]
+    print(text_to_print)
 
-        label = Label(fm, text=" Set bit depth")
-        label.pack(side=TOP, anchor=NW)
+# primary sample rate button functions
+def select_prisamplerate1_(var1):
+    selection = var1.get()
+    print('Primary Sample Rate', selection)
 
-        Setbit1_button = Button(fm, text="   16bit   ", command=self.Setbit1)
-        Setbit1_button.pack(side=TOP, anchor=NW)
+    text_dict = {
+        0: 'Nothing selected!',
+        14100: subprocess.call('currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 44100" /etc/pulse/daemon.conf', shell=True),
+        48000: subprocess.call('currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 48000" /etc/pulse/daemon.conf', shell=True),
+        88200: subprocess.call('currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 88200" /etc/pulse/daemon.conf', shell=True),
+        96000: subprocess.call('currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 96000" /etc/pulse/daemon.conf', shell=True),
+        192000: subprocess.call('currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 192000" /etc/pulse/daemon.conf', shell=True)
+        }
 
-        Setbit2_button = Button(fm, text="   24bit   ", command=self.Setbit2)
-        Setbit2_button.pack(side=TOP, anchor=NW)
-        fm.pack(fill=BOTH)
+    text_to_print = text_dict[selection]
+    print(text_to_print)
 
-        fm2 = Frame(master)
-        label = Label(fm2, text="Set primary sample rate ->    ")
-        label.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
+# secondary sample rate button functions
+def select_prisamplerate2_(var2):
+    selection = var2.get()
+    print('Secondary Sample Rate', selection)
 
-        Set1_button = Button(fm2, text="44,100 Hz", command=self.Set1)
-        Set1_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
+    text_dict = {
+        0: 'Nothing selected!',
+        14100: subprocess.call('currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 44100" /etc/pulse/daemon.conf', shell=True),
+        48000: subprocess.call('currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 48000" /etc/pulse/daemon.conf', shell=True),
+        88200: subprocess.call('currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 88200" /etc/pulse/daemon.conf', shell=True),
+        96000: subprocess.call('currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 96000" /etc/pulse/daemon.conf', shell=True),
+        192000: subprocess.call('currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 192000" /etc/pulse/daemon.conf', shell=True)
+        }
 
-        Set2_button = Button(fm2, text="48,000 Hz", command=self.Set2)
-        Set2_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
+    text_to_print = text_dict[selection]
+    print(text_to_print)
 
-        Set3_button = Button(fm2, text="88,200 Hz", command=self.Set3)
-        Set3_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
 
-        Set4_button = Button(fm2, text="96,000 Hz", command=self.Set4)
-        Set4_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
+# Render main  window
+def main():
+    root = tk.Tk()
+    root.title("GUI")
+    root.geometry("775x225")
 
-        Set5_button = Button(fm2, text="192,000 Hz", command=self.Set5)
-        Set5_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
-        fm2.pack(fill=BOTH)
+    # define var's
+    var = IntVar()
+    var1 = IntVar()
+    var2 = IntVar()
 
-        fm3 = Frame(master)
-        label = Label(master, text="Set alternative sample rate ->")
-        label.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
 
-        Setalt1_button = Button(master, text="44,100 Hz", command=self.Setalt1)
-        Setalt1_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
+    # background image
+    filename = PhotoImage(file = "desk03.gif")
+    background_label = Label( image=filename)
+    background_label.image = filename # anchor
+    background_label.place(x=70, y=-75, relwidth=1, relheight=1)
 
-        Setalt2_button = Button(master, text="48,000 Hz", command=self.Setalt2)
-        Setalt2_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
+    # Set bit depth
 
-        Setalt3_button = Button(master, text="88,200 Hz", command=self.Setalt3)
-        Setalt3_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
+    label = Label(text="Set bit depth")
+    label.grid(row=1, column=1)
 
-        Setalt4_button = Button(master, text="96,000 Hz", command=self.Setalt4)
-        Setalt4_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
+    select_bitdepth_1=Radiobutton(root, text='16 Bit', variable=var, command=lambda: select_bitdepth_(var), value=16, width=8)
+    select_bitdepth_1.grid(row=2, column=1)
 
-        Setalt5_button = Button(master, text="192,000 Hz", command=self.Setalt5)
-        Setalt5_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
-        fm3.pack(fill=BOTH)
+    select_bitdepth_2=Radiobutton(root, text='24 Bit', variable=var, command=lambda: select_bitdepth_(var), value=24, width=8)
+    select_bitdepth_2.grid(row=3, column=1)
 
-        ttk.Separator(root).place(x=0, y=131, relwidth=10)
+    select_bitdepth_3=Radiobutton(root, text='32 Bit', variable=var, command=lambda: select_bitdepth_(var), value=32, width=8)
+    select_bitdepth_3.grid(row=4, column=1)
 
-        fm4 = Frame(master)
-        label = Label(text="Resample method")
-        label.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
-        label.place(relx=1, x=-770, y=135)
+    # Separator1
+    ttk.Separator(root).place(x=0, y=77, relwidth=10)
 
-        SetRe1_button = Button(text="Default Settings", command=self.SetRe1)
-        SetRe1_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
-        SetRe1_button.place(relx=1, x=-774, y=195)
+    # Set primary sample rate
 
-        SetRe3_button = Button(master, text="medium quality", command=self.SetRe3)
-        SetRe3_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
-        SetRe3_button.place(relx=1, x=-495, y=134)
+    label = Label(text="Primary Sample rate")
+    label.grid(row=5, column=1, columnspan = 2,)
 
-        SetRe4_button = Button(master, text="best quality", command=self.SetRe4)
-        SetRe4_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
-        SetRe4_button.place(relx=1, x=-372, y=134)
+    select_prisamplerate_1=Radiobutton(root, text='44,100 Hz', variable=var1, command=lambda: select_prisamplerate1_(var1), value=14100, width=8)
+    select_prisamplerate_1.grid(row=5, column=3)
 
-        SetRe5_button = Button(master, text="zero order hold", command=self.SetRe5)
-        SetRe5_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
-        SetRe5_button.place(relx=1, x=-273, y=134)
+    select_prisamplerate_1=Radiobutton(root, text='48,000 Hz', variable=var1, command=lambda: select_prisamplerate1_(var1), value=48000, width=8)
+    select_prisamplerate_1.grid(row=5, column=4)
 
-        SetRe10_button = Button(master, text="ffmpeg", command=self.SetRe10)
-        SetRe10_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
-        SetRe10_button.place(relx=1, x=-566, y=134)
+    select_prisamplerate_1=Radiobutton(root, text='88,200 Hz', variable=var1, command=lambda: select_prisamplerate1_(var1), value=88200, width=8)
+    select_prisamplerate_1.grid(row=5, column=5)
 
-        SetRe12_button = Button(master, text="soxr-hq", command=self.SetRe12)
-        SetRe12_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
-        SetRe12_button.place(relx=1, x=-154, y=134)
+    select_prisamplerate_1=Radiobutton(root, text='96,000 Hz', variable=var1, command=lambda: select_prisamplerate1_(var1), value=96000, width=8)
+    select_prisamplerate_1.grid(row=5, column=6)
 
-        SetRe13_button = Button(master, text="soxr-vhq", command=self.SetRe13)
-        SetRe13_button.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
-        SetRe13_button.place(relx=1, x=-81, y=134)
-        fm4.pack(fill=BOTH)
+    select_prisamplerate_1=Radiobutton(root, text='192,000 Hz', variable=var1, command=lambda: select_prisamplerate1_(var1), value=192000, width=8)
+    select_prisamplerate_1.grid(row=5, column=7)
 
-        ttk.Separator(root).place(x=0, y=163, relwidth=10)
+    # Set Secondary sample rate
 
-        fm5 = Frame(master)
-        label = Label(master, text="Predefined settings")
-        label.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
-        label.place(relx=1, x=-770, y=171)
+    label = Label(text="Secondary Sample rate")
+    label.grid(row=6, column=1, columnspan = 2,)
 
-        Class1_button = Button(master, text="USB Audio: Class 1", command=self.Class1)
-        Class1_button.pack()
-        Class1_button.place(relx=1, x=-649, y=195)
+    select_secsamplerate_1=Radiobutton(root, text='44,100 Hz', variable=var2, command=lambda: select_prisamplerate2_(var2), value=14100, width=8)
+    select_secsamplerate_1.grid(row=6, column=3)
 
-        Class2_button = Button(master, text="USB Audio: Class 2", command=self.Class2)
-        Class2_button.pack()
-        Class2_button.place(relx=1, x=-508, y=195)
+    select_secsamplerate_1=Radiobutton(root, text='48,000 Hz', variable=var2, command=lambda: select_prisamplerate2_(var2), value=48000, width=8)
+    select_secsamplerate_1.grid(row=6, column=4)
 
-#        Test_button = Button(master, text="Show current sample rate", command=self.Test)
-#        Test_button.pack()
-#        Test_button.place(relx=1, x=-2, y=2, anchor=NE)
+    select_secsamplerate_1=Radiobutton(root, text='88,200 Hz', variable=var2, command=lambda: select_prisamplerate2_(var2), value=88200, width=8)
+    select_secsamplerate_1.grid(row=6, column=5)
 
-        Confirm_button = Button(master, text="   Apply !   ", command=self.Confirm)
-        Confirm_button.pack()
-        Confirm_button.place(relx=1, x=-160, y=195)
+    select_secsamplerate_1=Radiobutton(root, text='96,000 Hz', variable=var2, command=lambda: select_prisamplerate2_(var2), value=96000, width=8)
+    select_secsamplerate_1.grid(row=6, column=6)
 
-        close_button = Button(master, text="Close ", command=master.quit)
-        close_button.pack()
-        close_button.place(relx=1, x=-65, y=195)
-        fm5.pack(fill=BOTH)
+    select_secsamplerate_1=Radiobutton(root, text='192,000 Hz', variable=var2, command=lambda: select_prisamplerate2_(var2), value=192000, width=8)
+    select_secsamplerate_1.grid(row=6, column=7)
 
-    def Class1(self):
-           subprocess.call('currentbitrate=$(grep "default-sample-format" /etc/pulse/daemon.conf) && sudo sed -i "/${currentbitrate}/ c default-sample-format = s24le" /etc/pulse/daemon.conf && currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 96000" /etc/pulse/daemon.conf && currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 96000" /etc/pulse/daemon.conf', shell=True)
+    # Separator2
+    ttk.Separator(root).place(x=0, y=117, relwidth=10)
 
-    def Class2(self):
-           subprocess.call('currentbitrate=$(grep "default-sample-format" /etc/pulse/daemon.conf) && sudo sed -i "/${currentbitrate}/ c default-sample-format = s24le" /etc/pulse/daemon.conf && currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 192000" /etc/pulse/daemon.conf && currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 192000" /etc/pulse/daemon.conf', shell=True)
+    # Set Secondary sample rate
 
-    def Setbit1(self):
-           subprocess.call('currentbitrate=$(grep "default-sample-format" /etc/pulse/daemon.conf) && sudo sed -i "/${currentbitrate}/ c default-sample-format = s16le" /etc/pulse/daemon.conf', shell=True)
+    label = Label(text="Resample method")
+    label.grid(row=7, column=1, columnspan = 2,)
 
-    def Setbit2(self):
-           subprocess.call('currentbitrate=$(grep "default-sample-format" /etc/pulse/daemon.conf) && sudo sed -i "/${currentbitrate}/ c default-sample-format = s24le" /etc/pulse/daemon.conf', shell=True)
+#   Premade ENTER button with no actual function at now
+#    ser_port_btn=Button(root, text='ENTER', command=lambda: select_bitdepth_(var))
+#    ser_port_btn.grid(row=5, column=1)
 
+    root.mainloop()
 
-    def Set1(self):
-           subprocess.call('currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 44000" /etc/pulse/daemon.conf', shell=True)
-
-    def Set2(self):
-           subprocess.call('currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 48000" /etc/pulse/daemon.conf', shell=True)
-
-    def Set3(self):
-           subprocess.call('currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 88000" /etc/pulse/daemon.conf', shell=True)
-
-    def Set4(self):
-           subprocess.call('currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 96000" /etc/pulse/daemon.conf', shell=True)
-
-    def Set5(self):
-           subprocess.call('currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 192000" /etc/pulse/daemon.conf', shell=True)
-
-
-    def Setalt1(self):
-           subprocess.call('currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 44000" /etc/pulse/daemon.conf', shell=True)
-
-    def Setalt2(self):
-           subprocess.call('currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 48000" /etc/pulse/daemon.conf', shell=True)
-
-    def Setalt3(self):
-           subprocess.call('currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 88000" /etc/pulse/daemon.conf', shell=True)
-
-    def Setalt4(self):
-           subprocess.call('currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 96000" /etc/pulse/daemon.conf', shell=True)
-
-    def Setalt5(self):
-           subprocess.call('currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 192000" /etc/pulse/daemon.conf', shell=True)
-
-
-    def SetRe1(self):
-           subprocess.call('currentbitrate=$(grep "default-sample-format" /etc/pulse/daemon.conf) && sudo sed -i "/${currentbitrate}/ c default-sample-format = s16le" /etc/pulse/daemon.conf && currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c ; resample-method = speex-float-1" /etc/pulse/daemon.conf && currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c ; default-sample-rate = 44000" /etc/pulse/daemon.conf && currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 44000" /etc/pulse/daemon.conf', shell=True)
-
-    def SetRe3(self):
-           subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = src-sinc-medium-quality" /etc/pulse/daemon.conf', shell=True)
-
-    def SetRe4(self):
-           subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = src-sinc-best-quality" /etc/pulse/daemon.conf', shell=True)
-
-    def SetRe5(self):
-           subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = src-zero-order-hold" /etc/pulse/daemon.conf', shell=True)
-
-    def SetRe10(self):
-           subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = ffmpeg" /etc/pulse/daemon.conf', shell=True)
-
-    def SetRe12(self):
-           subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = soxr-hq" /etc/pulse/daemon.conf', shell=True)
-
-    def SetRe13(self):
-           subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = soxr-vhq" /etc/pulse/daemon.conf', shell=True)
-
-
-    def Test(self):
-           subprocess.call('pacmd list-sinks | grep sample', shell=True)
-
-    def Confirm(self):
-           subprocess.call('pulseaudio --kill && pulseaudio --start', shell=True)
-
-
-root = Tk()
-my_gui = AppGUI1(root)
-root.mainloop()
+if __name__ == '__main__':
+    main()
