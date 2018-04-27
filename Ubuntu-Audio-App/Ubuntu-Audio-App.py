d1da00ea0ebf802556d39299fae1f2b18281f34f
equal8888 <kimi.savander@gmail.com>
2018-04-27 14:15:08 +0300

    app with gui

--- Ubuntu-Audio-App/Ubuntu-Audio-App.py
+++ Ubuntu-Audio-App/Ubuntu-Audio-App.py
@@ -53,6 +53,30 @@ def select_prisamplerate2_(var2):
     text_to_print = text_dict[selection]
     print(text_to_print)
 
+# secondary sample rate button functions ("Monochrome"
+def select_prisamplerate3_(var3):
+    selection = var3.get()
+    print('Resample method', selection)
+
+    text_dict = {
+        0: 'Nothing selected!',
+        1: subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c ; resample-method = speex-float-1" /etc/pulse/daemon.conf', shell=True),
+        2: subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = speex-float-10" /etc/pulse/daemon.conf', shell=True),
+        3: subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = src-sinc-medium-quality" /etc/pulse/daemon.conf', shell=True),
+        4: subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = src-sinc-best-quality" /etc/pulse/daemon.conf', shell=True),
+        5: subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = src-zero-order-hold" /etc/pulse/daemon.conf', shell=True),
+        6: subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = src-linear" /etc/pulse/daemon.conf', shell=True),
+        7: subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = trivial" /etc/pulse/daemon.conf', shell=True),
+        8: subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = speex-float-N" /etc/pulse/daemon.conf', shell=True),
+        9: subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = speex-fixed-N" /etc/pulse/daemon.conf', shell=True),
+        10: subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = ffmpeg" /etc/pulse/daemon.conf', shell=True),
+        11: subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = soxr-mq" /etc/pulse/daemon.conf', shell=True),
+        12: subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = soxr-hq" /etc/pulse/daemon.conf', shell=True),
+        13: subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = soxr-vhq" /etc/pulse/daemon.conf', shell=True)
+}
+
+    text_to_print = text_dict[selection]
+    print(text_to_print)
 
 # Render main  window
 def main():
@@ -64,6 +88,7 @@ def main():
     var = IntVar()
     var1 = IntVar()
     var2 = IntVar()
+    var3 = IntVar()
 
 
     # background image
@@ -136,6 +161,49 @@ def main():
 
     label = Label(text="Resample method")
     label.grid(row=7, column=1, columnspan = 2,)
+
+    select_secsamplerate_1=Radiobutton(root, indicatoron=0, text='default', variable=var3, command=lambda: select_prisamplerate3_(var3), value=1, width=11)
+    select_secsamplerate_1.grid(row=7, column=3)
+
+    select_secsamplerate_1=Radiobutton(root, indicatoron=0, text='optimized', variable=var3, command=lambda: select_prisamplerate3_(var3), value=2, width=11)
+    select_secsamplerate_1.grid(row=7, column=4)
+
+    select_secsamplerate_1=Radiobutton(root, indicatoron=0, text='medium', variable=var3, command=lambda: select_prisamplerate3_(var3), value=3, width=11)
+    select_secsamplerate_1.grid(row=7, column=5)
+
+    select_secsamplerate_1=Radiobutton(root, indicatoron=0, text='best', variable=var3, command=lambda: select_prisamplerate3_(var3), value=4, width=11)
+    select_secsamplerate_1.grid(row=7, column=6)
+
+    select_secsamplerate_1=Radiobutton(root, indicatoron=0, text='zero-orderhold', variable=var3, command=lambda: select_prisamplerate3_(var3), value=5, width=11)
+    select_secsamplerate_1.grid(row=7, column=7)
+
+    select_secsamplerate_1=Radiobutton(root, indicatoron=0, text='src-linear', variable=var3, command=lambda: select_prisamplerate3_(var3), value=6, width=11)
+    select_secsamplerate_1.grid(row=7, column=8)
+
+    select_secsamplerate_1=Radiobutton(root, indicatoron=0, text='trivial', variable=var3, command=lambda: select_prisamplerate3_(var3), value=7, width=11)
+    select_secsamplerate_1.grid(row=8, column=1)
+
+    select_secsamplerate_1=Radiobutton(root, indicatoron=0, text='speex-float-N', variable=var3, command=lambda: select_prisamplerate3_(var3), value=8, width=11)
+    select_secsamplerate_1.grid(row=8, column=2)
+
+    select_secsamplerate_1=Radiobutton(root, indicatoron=0, text='speex-fixed-N', variable=var3, command=lambda: select_prisamplerate3_(var3), value=9, width=11)
+    select_secsamplerate_1.grid(row=8, column=3)
+
+    select_secsamplerate_1=Radiobutton(root, indicatoron=0, text='ffmpeg', variable=var3, command=lambda: select_prisamplerate3_(var3), value=10, width=11)
+    select_secsamplerate_1.grid(row=8, column=4)
+
+    select_secsamplerate_1=Radiobutton(root, indicatoron=0, text='soxr-mq', variable=var3, command=lambda: select_prisamplerate3_(var3), value=11, width=11)
+    select_secsamplerate_1.grid(row=8, column=5)
+
+    select_secsamplerate_1=Radiobutton(root, indicatoron=0, text='soxr-hq', variable=var3, command=lambda: select_prisamplerate3_(var3), value=12, width=11)
+    select_secsamplerate_1.grid(row=8, column=6)
+
+    select_secsamplerate_1=Radiobutton(root, indicatoron=0, text='soxr-vhq', variable=var3, command=lambda: select_prisamplerate3_(var3), value=13, width=11)
+    select_secsamplerate_1.grid(row=8, column=7)
+
+    # Separator3
+    ttk.Separator(root).place(x=0, y=158, relwidth=10)
+
 
 #   Premade ENTER button with no actual function at now
 #    ser_port_btn=Button(root, text='ENTER', command=lambda: select_bitdepth_(var))
