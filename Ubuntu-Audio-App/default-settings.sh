12114011d0546eb3d2ccd9cbd2a98c77ed2d71ab
equal8888 <kimi.savander@gmail.com>
2018-04-30 23:42:24 +0300

    default everything

--- Ubuntu-Audio-App/default-settings.sh
+++ Ubuntu-Audio-App/default-settings.sh
@@ -3,7 +3,7 @@
 # default alsa
 dmixmax=$(grep "# defaults.pcm.dmix.max_periods 0" /usr/share/alsa/alsa.conf) && sudo sed -i "/${dmixmax}/ c defaults.pcm.dmix.max_periods 0" /usr/share/alsa/alsa.conf &&
 dmixrate=$(grep "# defaults.pcm.dmix.rate 48000" /usr/share/alsa/alsa.conf) && sudo sed -i "/${dmixrate}/ c defaults.pcm.dmix.rate 48000" /usr/share/alsa/alsa.conf &&
-currentaltsamplerate=$(grep "# defaults.pcm.dmix.format" /usr/share/alsa/alsa.conf) && sudo sed -i "/${currentaltsamplerate}/ c defaults.pcm.dmix.format" /usr/share/alsa/alsa.conf &&
+currentaltsamplerate=$(grep "# defaults.pcm.dmix.format" /usr/share/alsa/alsa.conf) && sudo sed -i "/${currentaltsamplerate}/ c defaults.pcm.dmix.format 'unchanged'" /usr/share/alsa/alsa.conf &&
 dmixcard0=$(grep "# defaults.pcm.dmix.card defaults.pcm.card" /usr/share/alsa/alsa.conf) && sudo sed -i "/${dmixcard0}/ c defaults.pcm.dmix.card defaults.pcm.card" /usr/share/alsa/alsa.conf &&
 dmixcard2=$(grep "# defaults.pcm.dmix.device defaults.pcm.device" /usr/share/alsa/alsa.conf) && sudo sed -i "/${dmixcard2}/ c defaults.pcm.dmix.device defaults.pcm.device" /usr/share/alsa/alsa.conf &&
 dmixcard3=$(grep "# pcm.dmix cards.pcm.dmix" /usr/share/alsa/alsa.conf) && sudo sed -i "/${dmixcard3}/ c pcm.dmix cards.pcm.dmix" /usr/share/alsa/alsa.conf &&
