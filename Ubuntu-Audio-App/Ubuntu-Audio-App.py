2bf3250ed5289c7b19da7872cc4b4f1c11d5c5fd
equal8888 <kimi.savander@gmail.com>
2018-06-29 14:40:12 +0300

    minor changes

--- Ubuntu-Audio-App/Ubuntu-Audio-App.py
+++ Ubuntu-Audio-App/Ubuntu-Audio-App.py
@@ -68,7 +68,7 @@ def main():
     root = tk.Tk()
     root.title("Audio Powertool")
     root.minsize(width=730, height=288)
-    root.maxsize(width=730, height=288)
+#    root.maxsize(width=730, height=288)
     root.geometry("715x220")
 
     global password_box
@@ -148,16 +148,19 @@ def main():
 
     def select_bitdepth_16():
         subprocess.call('currentbitrate=$(grep "default-sample-format" /etc/pulse/daemon.conf) && sudo sed -i "/${currentbitrate}/ c default-sample-format = s16le" /etc/pulse/daemon.conf', shell=True)
+
     select_bitdepth_1=Radiobutton(frame99, command=select_bitdepth_16, text='16 Bit', variable=var, value=16, width=9)
     select_bitdepth_1.grid(row=2, column=1, sticky='N')
 
     def select_bitdepth_24():
         subprocess.call('currentbitrate=$(grep "default-sample-format" /etc/pulse/daemon.conf) && sudo sed -i "/${currentbitrate}/ c default-sample-format = s24le" /etc/pulse/daemon.conf', shell=True)
+
     select_bitdepth_1=Radiobutton(frame99, command=select_bitdepth_24, text='24 Bit', variable=var, value=24, width=9)
     select_bitdepth_1.grid(row=3, column=1, sticky='N')
 
     def select_bitdepth_32():
         subprocess.call('currentbitrate=$(grep "default-sample-format" /etc/pulse/daemon.conf) && sudo sed -i "/${currentbitrate}/ c default-sample-format = s32le" /etc/pulse/daemon.conf', shell=True)
+
     select_bitdepth_1=Radiobutton(frame99, command=select_bitdepth_32, text='32 Bit', variable=var, value=32, width=9)
     select_bitdepth_1.grid(row=4, column=1, sticky='N')
 
@@ -170,26 +173,31 @@ def main():
 
     def select_prisamplerate_44100():
         subprocess.call('currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 44100" /etc/pulse/daemon.conf', shell=True)
+
     select_prisamplerate_1=Radiobutton(frame99, command=select_prisamplerate_44100, text='44,100 Hz', variable=var1, value=14100, width=9)
     select_prisamplerate_1.grid(row=6, column=3)
 
     def select_prisamplerate_48000():
         subprocess.call('currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 48000" /etc/pulse/daemon.conf', shell=True)
+
     select_prisamplerate_1=Radiobutton(frame99, command=select_prisamplerate_48000, text='48,000 Hz', variable=var1, value=48000, width=9)
     select_prisamplerate_1.grid(row=6, column=4)
 
     def select_prisamplerate_88200():
         subprocess.call('currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 88200" /etc/pulse/daemon.conf', shell=True)
+
     select_prisamplerate_1=Radiobutton(frame99, command=select_prisamplerate_88200, text='88,200 Hz', variable=var1, value=88200, width=9)
     select_prisamplerate_1.grid(row=6, column=5)
 
     def select_prisamplerate_96000():
         subprocess.call('currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 96000" /etc/pulse/daemon.conf', shell=True)
+
     select_prisamplerate_1=Radiobutton(frame99, command=select_prisamplerate_96000, text='96,000 Hz', variable=var1, value=96000, width=9)
     select_prisamplerate_1.grid(row=6, column=6)
 
     def select_prisamplerate_192000():
         subprocess.call('currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c default-sample-rate = 192000" /etc/pulse/daemon.conf', shell=True)
+
     select_prisamplerate_1=Radiobutton(frame99, command=select_prisamplerate_192000, text='192,000 Hz', variable=var1, value=192000, width=9)
     select_prisamplerate_1.grid(row=6, column=7)
 
@@ -199,28 +207,33 @@ def main():
 
     def select_altsamplerate_44100():
         subprocess.call('currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 44100" /etc/pulse/daemon.conf', shell=True)
-    select_secsamplerate_1=Radiobutton(frame99, text='44,100 Hz', variable=var2, command=select_altsamplerate_44100, value=14100, width=9)
-    select_secsamplerate_1.grid(row=7, column=3)
+
+    select_altsamplerate_1=Radiobutton(frame99, text='44,100 Hz', variable=var2, command=select_altsamplerate_44100, value=14100, width=9)
+    select_altsamplerate_1.grid(row=7, column=3)
 
     def select_altsamplerate_48000():
         subprocess.call('currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 48000" /etc/pulse/daemon.conf', shell=True)
-    select_secsamplerate_1=Radiobutton(frame99, text='48,000 Hz', variable=var2, command=select_altsamplerate_48000, value=48000, width=9)
-    select_secsamplerate_1.grid(row=7, column=4)
+
+    select_altsamplerate_1=Radiobutton(frame99, text='48,000 Hz', variable=var2, command=select_altsamplerate_48000, value=48000, width=9)
+    select_altsamplerate_1.grid(row=7, column=4)
 
     def select_altsamplerate_88200():
         subprocess.call('currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 88200" /etc/pulse/daemon.conf', shell=True)
-    select_secsamplerate_1=Radiobutton(frame99, text='88,200 Hz', variable=var2, command=select_altsamplerate_88200, value=88200, width=9)
-    select_secsamplerate_1.grid(row=7, column=5)
+
+    select_altsamplerate_1=Radiobutton(frame99, text='88,200 Hz', variable=var2, command=select_altsamplerate_88200, value=88200, width=9)
+    select_altsamplerate_1.grid(row=7, column=5)
 
     def select_altsamplerate_96000():
         subprocess.call('currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 96000" /etc/pulse/daemon.conf', shell=True)
-    select_secsamplerate_1=Radiobutton(frame99, text='96,000 Hz', variable=var2, command=select_altsamplerate_96000, value=96000, width=9)
-    select_secsamplerate_1.grid(row=7, column=6)
+
+    select_altsamplerate_1=Radiobutton(frame99, text='96,000 Hz', variable=var2, command=select_altsamplerate_96000, value=96000, width=9)
+    select_altsamplerate_1.grid(row=7, column=6)
 
     def select_altsamplerate_192000():
         subprocess.call('currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c alternate-sample-rate = 192000" /etc/pulse/daemon.conf', shell=True)
-    select_secsamplerate_1=Radiobutton(frame99, text='192,000 Hz', variable=var2, command=select_altsamplerate_192000, value=192000, width=9)
-    select_secsamplerate_1.grid(row=7, column=7)
+
+    select_altsamplerate_1=Radiobutton(frame99, text='192,000 Hz', variable=var2, command=select_altsamplerate_192000, value=192000, width=9)
+    select_altsamplerate_1.grid(row=7, column=7)
 
     # Separator2
     ttk.Separator(frame99).grid(row=8, column=1, columnspan=11, sticky="ew")
@@ -231,63 +244,75 @@ def main():
 
     def select_prisamplerate_default():
         subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c ; resample-method = speex-float-1" /etc/pulse/daemon.conf', shell=True)
-    select_secsamplerate_1=Radiobutton(frame99, indicatoron=0, text='default', variable=var3, command=select_prisamplerate_default, value=1, width=12)
-    select_secsamplerate_1.grid(row=9, column=3)
+
+    select_resamplerate_1=Radiobutton(frame99, indicatoron=0, text='default', variable=var3, command=select_prisamplerate_default, value=1, width=12)
+    select_resamplerate_1.grid(row=9, column=3)
 
     def select_prisamplerate_optimised():
         subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = speex-float-10" /etc/pulse/daemon.conf', shell=True)
-    select_secsamplerate_1=Radiobutton(frame99, indicatoron=0, text='fast', variable=var3, command=select_prisamplerate_optimised, value=2, width=12)
-    select_secsamplerate_1.grid(row=9, column=4)
+
+    select_resamplerate_1=Radiobutton(frame99, indicatoron=0, text='fast', variable=var3, command=select_prisamplerate_optimised, value=2, width=12)
+    select_resamplerate_1.grid(row=9, column=4)
 
     def select_prisamplerate_srcsincmediumquality():
         subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = src-sinc-medium-quality" /etc/pulse/daemon.conf', shell=True)
-    select_secsamplerate_1=Radiobutton(frame99, indicatoron=0, text='medium', variable=var3, command=select_prisamplerate_srcsincmediumquality, value=3, width=12)
-    select_secsamplerate_1.grid(row=9, column=5)
+
+    select_resamplerate_1=Radiobutton(frame99, indicatoron=0, text='medium', variable=var3, command=select_prisamplerate_srcsincmediumquality, value=3, width=12)
+    select_resamplerate_1.grid(row=9, column=5)
 
     def select_prisamplerate_srcsincbestquality():
         subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = src-sinc-best-quality" /etc/pulse/daemon.conf', shell=True)
-    select_secsamplerate_1=Radiobutton(frame99, indicatoron=0, text='best', variable=var3, command=select_prisamplerate_srcsincbestquality, value=4, width=12)
-    select_secsamplerate_1.grid(row=9, column=6)
+
+    select_resamplerate_1=Radiobutton(frame99, indicatoron=0, text='best', variable=var3, command=select_prisamplerate_srcsincbestquality, value=4, width=12)
+    select_resamplerate_1.grid(row=9, column=6)
 
     def select_prisamplerate_srczeroorderhold():
         subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = src-zero-order-hold" /etc/pulse/daemon.conf', shell=True)
-    select_secsamplerate_1=Radiobutton(frame99, indicatoron=0, text='zero-orderhold', variable=var3, command=select_prisamplerate_srczeroorderhold, value=5, width=12)
-    select_secsamplerate_1.grid(row=9, column=7)
+
+    select_resamplerate_1=Radiobutton(frame99, indicatoron=0, text='zero-orderhold', variable=var3, command=select_prisamplerate_srczeroorderhold, value=5, width=12)
+    select_resamplerate_1.grid(row=9, column=7)
 
     def select_prisamplerate_srclinear():
         subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = src-linear" /etc/pulse/daemon.conf', shell=True)
-    select_secsamplerate_1=Radiobutton(frame99, indicatoron=0, text='src-linear', variable=var3, command=select_prisamplerate_srclinear, value=6, width=12)
-    select_secsamplerate_1.grid(row=10, column=5)
+
+    select_resamplerate_1=Radiobutton(frame99, indicatoron=0, text='src-linear', variable=var3, command=select_prisamplerate_srclinear, value=6, width=12)
+    select_resamplerate_1.grid(row=10, column=5)
 
     def select_prisamplerate_trivial():
         subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = trivial" /etc/pulse/daemon.conf', shell=True)
-    select_secsamplerate_1=Radiobutton(frame99, indicatoron=0, text='trivial', variable=var3, command=select_prisamplerate_trivial, value=7, width=12)
-    select_secsamplerate_1.grid(row=10, column=1)
+
+    select_resamplerate_1=Radiobutton(frame99, indicatoron=0, text='trivial', variable=var3, command=select_prisamplerate_trivial, value=7, width=12)
+    select_resamplerate_1.grid(row=10, column=1)
 
     def select_prisamplerate_speexfloatN():
         subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = speex-float-N" /etc/pulse/daemon.conf', shell=True)
-    select_secsamplerate_1=Radiobutton(frame99, indicatoron=0, text='speex-float-N', variable=var3, command=select_prisamplerate_speexfloatN, value=8, width=12)
-    select_secsamplerate_1.grid(row=10, column=2)
+
+    select_resamplerate_1=Radiobutton(frame99, indicatoron=0, text='speex-float-N', variable=var3, command=select_prisamplerate_speexfloatN, value=8, width=12)
+    select_resamplerate_1.grid(row=10, column=2)
 
     def select_prisamplerate_speexfixedN():
         subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = speex-fixed-N" /etc/pulse/daemon.conf', shell=True)
-    select_secsamplerate_1=Radiobutton(frame99, indicatoron=0, text='speex-fixed-N', variable=var3, command=select_prisamplerate_speexfixedN, value=9, width=12)
-    select_secsamplerate_1.grid(row=10, column=3)
+
+    select_resamplerate_1=Radiobutton(frame99, indicatoron=0, text='speex-fixed-N', variable=var3, command=select_prisamplerate_speexfixedN, value=9, width=12)
+    select_resamplerate_1.grid(row=10, column=3)
 
     def select_prisamplerate_ffmpeg():
         subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = ffmpeg" /etc/pulse/daemon.conf', shell=True)
-    select_secsamplerate_1=Radiobutton(frame99, indicatoron=0, text='ffmpeg', variable=var3, command=select_prisamplerate_ffmpeg, value=10, width=12)
-    select_secsamplerate_1.grid(row=10, column=4)
+
+    select_resamplerate_1=Radiobutton(frame99, indicatoron=0, text='ffmpeg', variable=var3, command=select_prisamplerate_ffmpeg, value=10, width=12)
+    select_resamplerate_1.grid(row=10, column=4)
 
     def select_prisamplerate_soxrhq():
         subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = soxr-hq" /etc/pulse/daemon.conf', shell=True)
-    select_secsamplerate_1=Radiobutton(frame99, indicatoron=0, text='soxr-hq', variable=var3, command=select_prisamplerate_soxrhq, value=12, width=12)
-    select_secsamplerate_1.grid(row=10, column=6)
+
+    select_resamplerate_1=Radiobutton(frame99, indicatoron=0, text='soxr-hq', variable=var3, command=select_prisamplerate_soxrhq, value=12, width=12)
+    select_resamplerate_1.grid(row=10, column=6)
 
     def select_prisamplerate_soxrvhq():
         subprocess.call('currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c resample-method = soxr-vhq" /etc/pulse/daemon.conf', shell=True)
-    select_secsamplerate_1=Radiobutton(frame99, indicatoron=0, text='soxr-vhq', variable=var3, command=select_prisamplerate_soxrvhq, value=13, width=12)
-    select_secsamplerate_1.grid(row=10, column=7)
+
+    select_resamplerate_1=Radiobutton(frame99, indicatoron=0, text='soxr-vhq', variable=var3, command=select_prisamplerate_soxrvhq, value=13, width=12)
+    select_resamplerate_1.grid(row=10, column=7)
 
     # Separator3 usb1button
     ttk.Separator(frame99).grid(row=11, column=1, columnspan=11, sticky="ew")
@@ -298,13 +323,13 @@ def main():
 
 #   default button for now
     def defaultpulsebutton():
-        subprocess.call('cd ~ && ./Documents/gitfix/Ubuntu-Audio-App/default-settings-pulseaudio.sh', shell=True)
+        subprocess.call('defaultsettingspath=$(realpath --relative-base=$HOME default-settings-pulseaudio.sh) && realdefaultsettingspath="~/"$defaultsettingspath && eval $realdefaultsettingspath', shell=True)
     apply_btn1=Button(frame99, text='Default Pulseaudio settings', command=defaultpulsebutton )
     apply_btn1.grid(row=12, column=1, columnspan=2, padx=5, pady=5)
 
 #   restart pulseaudio
     def restartpulse():
-        subprocess.call('pulsespawn=$(grep "autospawn" /etc/pulse/client.conf) && sudo sed -i "/${pulsespawn}/ c ; autospawn = yes" /etc/pulse/client.conf && pulseaudio --kill ; pulseaudio --start', shell=True)
+        subprocess.call('pulseaudio --kill ; pulseaudio --start', shell=True)
     apply_btn2=Button(frame99, text='Apply / Restart pulseaudio', command=restartpulse )
     apply_btn2.grid(row=12, column=6, columnspan=4, padx=5, pady=5)
 
@@ -312,7 +337,8 @@ def main():
     def showsamplerate():
         showsamplerateoutput = subprocess.check_output(["(pacmd list-sinks | grep sample)"], shell=True)
         bitdepthtextvariable.set (showsamplerateoutput)
-    apply_btn3=Button(frame99, text='Show current PA sample rate', command=showsamplerate )
+
+    apply_btn3=Button(frame99, text='Test for possiple resampling', command=showsamplerate )
     apply_btn3.grid(row=1, column=6, sticky='E', rowspan=2, columnspan=2, padx=3, pady=2)
 
 # PAGE2 Maincode
