dd3a32f1a1b5f0fef85cdc9de92d34b21733ed97
equal8888 <kimi.savander@gmail.com>
2018-11-04 05:32:07 +0200

    faster script

--- Ubuntu-Audio-App/default-settings-pulseaudio.sh
+++ Ubuntu-Audio-App/default-settings-pulseaudio.sh
@@ -3,17 +3,18 @@
 # default pulseaudio config
 
 # default autospawn
-pulsespawn=$(grep "autospawn" /etc/pulse/client.conf) && sudo sed -i "/${pulsespawn}/ c ; autospawn = yes" /etc/pulse/client.conf &&
+sudo sed -i "/autospawn =/ c ; autospawn = yes" /etc/pulse/client.conf &&
 
 # default bitdepth
-currentbitrate=$(grep "default-sample-format" /etc/pulse/daemon.conf) && sudo sed -i "/${currentbitrate}/ c ; default-sample-format = s16le" /etc/pulse/daemon.conf &&
+sudo sed -i "/default-sample-format =/ c ; default-sample-format = s16le" /etc/pulse/daemon.conf &&
 
 # default sample rate's
-currentsamplerate=$(grep "default-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentsamplerate}/ c ; default-sample-rate = 44100" /etc/pulse/daemon.conf &&
-currentaltsamplerate=$(grep "alternate-sample-rate" /etc/pulse/daemon.conf) && sudo sed -i "/${currentaltsamplerate}/ c ; alternate-sample-rate = 44100" /etc/pulse/daemon.conf &&
+sudo sed -i "/default-sample-rate =/ c ; default-sample-rate = 44100" /etc/pulse/daemon.conf &&
+
+sudo sed -i "/alternate-sample-rate =/ c ; alternate-sample-rate = 44100" /etc/pulse/daemon.conf &&
 
 # default resample
-currentresamplerate=$(grep "resample-method" /etc/pulse/daemon.conf) && sudo sed -i "/${currentresamplerate}/ c ; resample-method = speex-float-1" /etc/pulse/daemon.conf &&
+sudo sed -i "/resample-method =/ c ; resample-method = speex-float-1" /etc/pulse/daemon.conf &&
 
 # restart pulseaudio
 pulseaudio --kill ; pulseaudio --start &&
@@ -26,5 +27,5 @@ printf "${GREEN} Pulseaudio${NC} Set to default !\n" &&
 
 RED='\033[0;31m'
 NC='\033[0m' # No Color
-printf "${RED} Services${NC} Restarting (wait 5-10 seconds)\n" &&
+printf "${RED} Services${NC} Restarting (wait 3 seconds)\n" &&
 echo "-------------------------------------------------------------"
