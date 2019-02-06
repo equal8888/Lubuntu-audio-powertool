f129053445e331ea83ddb37f3420211892f2678c
equal8888 <kimi.savander@gmail.com>
2019-02-06 12:03:37 +0200

    fixes

--- Ubuntu-Audio-App/Ubuntu-Audio-App.py
+++ Ubuntu-Audio-App/Ubuntu-Audio-App.py
@@ -23,7 +23,7 @@ def main():
 
 	# Menu for pages
     nb=ttk.Notebook(mainFrame.master)
-    nb.grid(row=1,column=1,columnspan=50,rowspan=49,sticky='NESW')
+    nb.grid(row=1,column=1,sticky='NESW')
 
 	# Page 1 PulseAudio
     page1=ttk.Frame(nb)
@@ -39,7 +39,7 @@ def main():
 
     # page1 main frame
     frame0=tkinter.LabelFrame(page1,bd=6,bg="blue3")
-    frame0.grid(row=0,column=0,columnspan=9,rowspan=2,sticky='nesw')
+    frame0.grid(row=0,column=0,sticky='nesw')
 
     # page1 frame
     frame1=tkinter.LabelFrame(frame0,bd=5,bg="black")
@@ -110,40 +110,40 @@ def main():
 # End ----------
 
     # Separator
-    ttk.Separator(page1).grid(row=2,column=0,columnspan=11,sticky="ew")
+    ttk.Separator(page1).grid(row=2,column=0,sticky="ew")
 
 # ---------- Primary Samplerate Radio Buttons ----------
 
 	# samplerate frame
     frame3=Label(page1)
-    frame3.grid(row=5,column=0,columnspan=8,sticky='nesw')
+    frame3.grid(row=5,column=0,sticky='nesw')
 
 	# "Sub frame" for samplerate frame
     frame4=Label(frame3)
-    frame4.grid(row=5,column=0,columnspan=8,sticky='nesw')
+    frame4.grid(row=5,column=0,sticky='nesw')
 
 	# Label
     label=Label(frame4,text="2) Primary Sample rate")
     label.grid(row=1,column=1)
 
 	# Set the BithDepth value to default for variable vPaBitdepth
-    RadPriRateDefault=Radiobutton(frame4,text='Default',variable=vPaPriRate,value='; default-sample-rate = 44100',command=PaPriRate,width=9)
+    RadPriRateDefault=Radiobutton(frame4,text='Default',variable=vPaPriRate,value='; default-sample-rate = 44100',command=PaPriRate,width=11)
     RadPriRateDefault.grid(row=1,column=2)
 
 	# Set the BithDepth value to 44,100 Hz for variable vPaBitdepth
-    RadPriRate44100=Radiobutton(frame4,text='44,100 Hz',variable=vPaPriRate,value='  default-sample-rate = 44100',command=PaPriRate,width=9)
+    RadPriRate44100=Radiobutton(frame4,text='44,100 Hz',variable=vPaPriRate,value='  default-sample-rate = 44100',command=PaPriRate,width=11)
     RadPriRate44100.grid(row=1,column=3)
 
 	# Set the BithDepth value to 48,000 Hz for variable vPaBitdepth
-    RadPriRate48000=Radiobutton(frame4,text='48,000 Hz',variable=vPaPriRate,value='  default-sample-rate = 48000',command=PaPriRate,width=9)
+    RadPriRate48000=Radiobutton(frame4,text='48,000 Hz',variable=vPaPriRate,value='  default-sample-rate = 48000',command=PaPriRate,width=11)
     RadPriRate48000.grid(row=1,column=4)
 
 	# Set the BithDepth value to 88,200 Hz for variable vPaBitdepth
-    RadPriRate88200=Radiobutton(frame4,text='88,200 Hz',variable=vPaPriRate,value='  default-sample-rate = 88200',command=PaPriRate,width=9)
+    RadPriRate88200=Radiobutton(frame4,text='88,200 Hz',variable=vPaPriRate,value='  default-sample-rate = 88200',command=PaPriRate,width=11)
     RadPriRate88200.grid(row=1,column=5)
 
 	# Set the BithDepth value to 96,000 Hz for variable vPaBitdepth
-    RadPriRate96000=Radiobutton(frame4,text='96,000 Hz',variable=vPaPriRate,value='  default-sample-rate = 96000',command=PaPriRate,width=9)
+    RadPriRate96000=Radiobutton(frame4,text='96,000 Hz',variable=vPaPriRate,value='  default-sample-rate = 96000',command=PaPriRate,width=11)
     RadPriRate96000.grid(row=1,column=6)
 
 # End ----------
@@ -155,29 +155,29 @@ def main():
     label.grid(row=2,column=1)
 
 	# Set the BithDepth value to default for variable vPaAltRate
-    RadAltRateDefault=Radiobutton(frame4,text='Default',variable=vPaAltRate,value='; alternate-sample-rate = 48000',command=PaAltRate,width=9)
+    RadAltRateDefault=Radiobutton(frame4,text='Default',variable=vPaAltRate,value='; alternate-sample-rate = 48000',command=PaAltRate,width=11)
     RadAltRateDefault.grid(row=2,column=2)
 
 	# Set the BithDepth value to 48,000 Hz for variable vPaAltRate
-    RadAltRate48000=Radiobutton(frame4,text='44,100 Hz',variable=vPaAltRate,value='  alternate-sample-rate = 44100',command=PaAltRate,width=9)
+    RadAltRate48000=Radiobutton(frame4,text='44,100 Hz',variable=vPaAltRate,value='  alternate-sample-rate = 44100',command=PaAltRate,width=11)
     RadAltRate48000.grid(row=2,column=3)
 
 	# Set the BithDepth value to 88,200 Hz for variable vPaAltRate
-    RadAltRate48000=Radiobutton(frame4,text='48,000 Hz',variable=vPaAltRate,value='  alternate-sample-rate = 48000',command=PaAltRate,width=9)
+    RadAltRate48000=Radiobutton(frame4,text='48,000 Hz',variable=vPaAltRate,value='  alternate-sample-rate = 48000',command=PaAltRate,width=11)
     RadAltRate48000.grid(row=2,column=4)
 
 	# Set the BithDepth value to 96,000 Hz for variable vPaAltRate
-    RadAltRate96000=Radiobutton(frame4,text='88,200 Hz',variable=vPaAltRate,value='  alternate-sample-rate = 88200',command=PaAltRate,width=9)
+    RadAltRate96000=Radiobutton(frame4,text='88,200 Hz',variable=vPaAltRate,value='  alternate-sample-rate = 88200',command=PaAltRate,width=11)
     RadAltRate96000.grid(row=2,column=5)
 
 	# Set the BithDepth value to 192,000 Hz for variable vPaAltRate
-    RadAltRate192000=Radiobutton(frame4,text='96,000 Hz',variable=vPaAltRate,value='  alternate-sample-rate = 96000',command=PaAltRate,width=9)
+    RadAltRate192000=Radiobutton(frame4,text='96,000 Hz',variable=vPaAltRate,value='  alternate-sample-rate = 96000',command=PaAltRate,width=11)
     RadAltRate192000.grid(row=2,column=6)
 
 # End ----------
 
     # Separator-2
-    ttk.Separator(page1).grid(row=8,column=0,columnspan=11,sticky="ew")
+    ttk.Separator(page1).grid(row=8,column=0,sticky="ew")
 
 # ---------- Resample method Radio Buttons ----------
     frame5=Label(page1)
@@ -188,52 +188,46 @@ def main():
     label=Label(frame5,text="4) Resample method")
     label.grid(row=1,column=1,pady=4)
 
-#    vPaRespeexfloatN=Radiobutton(page1,indicatoron=0,text='???',variable=vPaRe,value='resample-method = speex-float-N',command=PaRe,width=12)
-#    vPaRespeexfloatN.grid(row=9,column=3)
-
 	# Set the Resample value to speexfloat-10 for variable vPaRe
-    vPaRespeexfloat10=Radiobutton(frame5,indicatoron=0,text='speexfloat-10',variable=vPaRe,value='  resample-method = speex-float-10',command=PaRe,width=12)
+    vPaRespeexfloat10=Radiobutton(frame5,indicatoron=0,text='speexfloat-10',variable=vPaRe,value='  resample-method = speex-float-10',command=PaRe,width=16)
     vPaRespeexfloat10.grid(row=1,column=4)
 
 	# Set the Resample value to medium for variable vPaRe
-    vPaRemedium=Radiobutton(frame5,indicatoron=0,text='medium',variable=vPaRe,value='  resample-method = src-sinc-medium-quality',command=PaRe,width=12)
+    vPaRemedium=Radiobutton(frame5,indicatoron=0,text='medium',variable=vPaRe,value='  resample-method = src-sinc-medium-quality',command=PaRe,width=16)
     vPaRemedium.grid(row=1,column=5)
 
 	# Set the Resample value to best for variable vPaRe
-    vPaRebest=Radiobutton(frame5,indicatoron=0,text='best',variable=vPaRe,value='  resample-method = src-sinc-best-quality',command=PaRe,width=12)
+    vPaRebest=Radiobutton(frame5,indicatoron=0,text='best',variable=vPaRe,value='  resample-method = src-sinc-best-quality',command=PaRe,width=16)
     vPaRebest.grid(row=1,column=6)
 
 	# Set the Resample value to zero-orderhold for variable vPaRe
-    vPaRezeroOrderhold=Radiobutton(frame5,indicatoron=0,text='zero-orderhold',variable=vPaRe,value='  resample-method = src-zero-order-hold',command=PaRe,width=12)
+    vPaRezeroOrderhold=Radiobutton(frame5,indicatoron=0,text='zero-orderhold',variable=vPaRe,value='  resample-method = src-zero-order-hold',command=PaRe,width=16)
     vPaRezeroOrderhold.grid(row=1, column=7)
 
 	# Set the Resample value to Default Resampling for variable vPaRe
     vPaReStopResampling=Radiobutton(frame5,indicatoron=0,text='Default Resampling',variable=vPaRe,value='; resample-method = speex-float-1',command=PaRe,width=25)
-    vPaReStopResampling.grid(row=2,column=1,columnspan=2)
-
-#    vPaReresamplemethod=Radiobutton(page1,indicatoron=0,text='???',variable=vPaRe,value='resample-method = speex-fixed-N',command=PaRe,width=12)
-#    vPaReresamplemethod.grid(row=10,column=3)
+    vPaReStopResampling.grid(row=2,column=1)
 
 	# Set the Resample value to ffmpeg for variable vPaRe
-    vPaReffmpeg=Radiobutton(frame5,indicatoron=0,text='ffmpeg',variable=vPaRe,value='  resample-method = ffmpeg',command=PaRe,width=12)
+    vPaReffmpeg=Radiobutton(frame5,indicatoron=0,text='ffmpeg',variable=vPaRe,value='  resample-method = ffmpeg',command=PaRe,width=16)
     vPaReffmpeg.grid(row=2,column=4)
 
 	# Set the Resample value to src-linear for variable vPaRe
-    vPaResrclinear=Radiobutton(frame5,indicatoron=0,text='src-linear',variable=vPaRe,value='  resample-method = src-linear',command=PaRe,width=12)
+    vPaResrclinear=Radiobutton(frame5,indicatoron=0,text='src-linear',variable=vPaRe,value='  resample-method = src-linear',command=PaRe,width=16)
     vPaResrclinear.grid(row=2,column=5)
 
 	# Set the Resample value to soxr-hq for variable vPaRe
-    vPaResoxrhq=Radiobutton(frame5,indicatoron=0,text='soxr-hq',variable=vPaRe,value='  resample-method = soxr-hq',command=PaRe,width=12)
+    vPaResoxrhq=Radiobutton(frame5,indicatoron=0,text='soxr-hq',variable=vPaRe,value='  resample-method = soxr-hq',command=PaRe,width=16)
     vPaResoxrhq.grid(row=2,column=6)
 
 	# Set the Resample value to soxr-vhq for variable vPaRe
-    vPaResoxrvhq=Radiobutton(frame5,indicatoron=0,text='soxr-vhq',variable=vPaRe,value='  resample-method = soxr-vhq',command=PaRe,width=12)
+    vPaResoxrvhq=Radiobutton(frame5,indicatoron=0,text='soxr-vhq',variable=vPaRe,value='  resample-method = soxr-vhq',command=PaRe,width=16)
     vPaResoxrvhq.grid(row=2,column=7)
 
 # End ----------
 
     # Separator3
-    ttk.Separator(page1).grid(row=11,column=0,columnspan=11,sticky="ew")
+    ttk.Separator(page1).grid(row=11,column=0,sticky="ew")
 
 	# Set the Buttons (Default Settings)
     frame5=Label(page1)
@@ -255,10 +249,10 @@ def main():
     RadPul01.grid(row=13,column=1,sticky='nsw')
 
 	# Select to uninstall Palemoon
-    RadPul02=Radiobutton(RemPa11,text='Uninstall PulseAudio',variable=vPaUninst,value='pulseaudio -k | killall pulseaudio && sudo apt-get purge padevchooser -y && sudo apt-get purge paprefs -y && sudo apt-get purge pulseaudio gstreamer0.10-pulseaudio -y && sudo apt-get purge pulseaudio-utils -y && sudo apt autoremove -y',command=ADefDev)
+    RadPul02=Radiobutton(RemPa11,text='Uninstall PulseAudio',variable=vPaUninst,value='pulseaudio -k | killall pulseaudio && sudo apt-get purge padevchooser -y && sudo apt-get purge paprefs -y && sudo apt-get purge pulseaudio gstreamer0.10-pulseaudio -y && sudo apt-get purge pulseaudio-utils -y && sudo apt-get install lxde -y && sudo apt install lxde-common && sudo apt autoremove -y',command=ADefDev)
     RadPul02.grid(row=13,column=2,sticky='nsw')
 
-    AplPul1=Button(RemPa11,text='Install / Uninstall',command=installerPA)
+    AplPul1=Button(RemPa11,text='Apply Install / Uninstall',command=installerPA)
     AplPul1.grid(row=14,column=1,columnspan=4,padx=5,pady=5,sticky='nesw')
 
 # End ----------
@@ -278,18 +272,6 @@ def main():
 # End ----------
     apply_btn11=Button(frame5,text='Set all PulseAudio Values to Default',command=defaultpulsebutton)
     apply_btn11.grid(row=1,column=3,padx=5)
-
-
-
-#    apply_btn3=Button(frame5,text='Remove PulseAudio',foreground="red4",command=RemPa)
-#    apply_btn3.grid(row=13,column=4,padx=5,pady=5)
-
-# End ----------
-
-# Text below app
-#    label=Label(frame6,text="Press apply for changes to take affect  ")
-#    label.grid(row=1,column=0,padx=5)
-
 
 # Button Show Samplerate
     apply_btn3=Button(frame1,text='PulseAudio Status & Output (Click to refresh)',command=showsamplerate)
@@ -359,6 +341,7 @@ def main():
 
 # End ----------
 
+
 # ---------- Install Palemoon Browser ----------
 
 	# Select to install Palemoon
@@ -370,14 +353,14 @@ def main():
     RadAL01.grid(row=2,column=1,sticky='nsw')
 
     AplAL1=Button(frame103,text='Apply',command=applyAL,state=DISABLED)
-    AplAL1.grid(row=8,column=0,columnspan=4,padx=5,pady=5,sticky='nesw')
+    AplAL1.grid(row=8,column=0,padx=5,pady=5,sticky='nesw')
 
 # End ----------
 
 # ---------- ALSA Apply ----------
 
     AplAL1=Button(frame101,text='Apply & Restart ALSA',command=applyAL)
-    AplAL1.grid(row=8,column=0,columnspan=4,padx=5,pady=5,sticky='nesw')
+    AplAL1.grid(row=8,column=0,padx=5,pady=5,sticky='nesw')
 
 # End ----------
 
@@ -500,10 +483,6 @@ def applyPA():
     CvPaRe=(vPaRe.get())
 
     subprocess.call('sudo sed -i "/default-sample-format =/ c {}" /etc/pulse/daemon.conf && sudo sed -i "/default-sample-rate =/ c {}" /etc/pulse/daemon.conf && sudo sed -i "/alternate-sample-rate =/ c {}" /etc/pulse/daemon.conf && sudo sed -i "/resample-method =/ c {}" /etc/pulse/daemon.conf | pulseaudio --kill ; pulseaudio --start'.format(CvPaBitdepth,CvPaPriRate,CvPaAltRate,CvPaRe),shell=True);
-
-# Remove PA
-# def RemPa():
-#    subprocess.call('sudo apt-get autoremove pulseaudio && sudo apt-get install lxde -y && sudo apt-get install lubuntu-desktop -y && sudo alsa force-reload', shell=True)
 
 # The current PulseAudio output setting is passed to variable and printed to terminal
 def showsamplerate():
